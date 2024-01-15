e6e6b397a7e3c3ae30fb112fb1fcbeaed9110310
정훈 <vision981111@gmail.com>
2024-01-15 17:54:32 +0900

    0115집에서 공부하자

---
+++ 01_python/python01/python_hw_1_4/hw_1_4.py
@@ -0,0 +1,16 @@
+# 사과는 영어로 apple
+# 바나나는 영어로 banana
+# 키위는 영어로 kiwi
+# ap='apple'
+# ba='banana'
+# ki='kiwi'
+# print('사과는 영어로 '+ap)
+# print('바나나는 영어로 '+ba)
+# print('키위는 영어로 '+ki)
+apple_is = '사과는 영어로 apple'
+banana_is = '바나나는 영어로 banana'
+kiwi_is = '키위는 영어로 kiwi'
+
+print(apple_is)
+print(banana_is)
+print(kiwi_is)
---
+++ 01_python/python01/python_ws_1_4/ws_1_4.py
@@ -0,0 +1,12 @@
+# 원주율
+PI = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679
+# 반지름
+radius = 15
+pi_is = '원주율 : '
+radius_is = '반지름 : '
+circle_c = '원의 둘레 : '
+circle_s = '원의 넓이 : '
+print(pi_is, PI)
+print(radius_is, radius)
+print(circle_c,radius*PI*2)
+print(circle_s,radius**2*PI)
