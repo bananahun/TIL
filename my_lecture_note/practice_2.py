8e75c6668163c08831f0dcd549da998eec0be2a4
bananahun <vision981111@gmail.com>
2024-01-21 21:07:25 +0900

    0121homestudy

--- my_lecture_note/practice_2.py
+++ my_lecture_note/practice_2.py
@@ -7,3 +7,19 @@ def greet(name):
 result = greet('Arsenal')
 print(result)
 
+dust = 480
+
+if dust > 150:
+    print('매우 나쁨')
+    """
+    중첩 조건문
+    """
+    if dust > 300:
+        print('위험해요! 나가지 마세요!')
+elif dust > 80:
+    print('나쁨')
+elif dust > 30:
+    print('보통')
+else:
+    print('좋음')
+
