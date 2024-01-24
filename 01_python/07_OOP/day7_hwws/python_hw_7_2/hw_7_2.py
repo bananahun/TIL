00f508b3c8fa88db2c9ac9f1ddbdd79e95b98e31
정훈 <vision981111@gmail.com>
2024-01-24 15:32:47 +0900

    0124진짜 어렵다

---
+++ 01_python/07_OOP/day7_hwws/python_hw_7_2/hw_7_2.py
@@ -0,0 +1,9 @@
+# 아래 클래스를 수정하시오.
+class StringRepeater:
+    def repeat_string(self, num, string):
+        for m in range(num):
+            print(string)
+        
+repeater1 = StringRepeater()
+repeater1.repeat_string(3,'hello')
+
