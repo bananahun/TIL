a4b8b0da5b51c1cd39c02aee466cbcd89f224cfe
정훈 <vision981111@naver.com>
2024-04-05 17:39:03 +0900

    go home

---
+++ algorithm/boj/class_3/5525_IOIOI/hun.py
@@ -0,0 +1,11 @@
+import sys
+sys.stdin = open('input.txt')
+input = sys.stdin.readline
+
+N = int(input())
+length = int(input())
+arr = list(input())
+# print(arr)
+for i in range(length):
+    if arr[i] == 'I'
+    
