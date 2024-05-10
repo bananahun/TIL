56af28bd0198f5482f83801d434a037bd8e0fd98
정훈 <vision981111@naver.com>
2024-05-10 18:00:19 +0900

    집에가자

--- academy/99_pjt/01PJT_FINANCE/01PJT.py
+++
@@ -1,7 +0,0 @@
-import requests
-import pprint
-
-def get_deposit_products():
-    api_key = 'edc13e5c85c71c8195da70044d66f4f2'
-
-    url = 
--- academy/99_pjt/07_pjt/finlife/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import DepositOptions, DepositProducts
-# Register your models here.
-admin.site.register(DepositOptions)
-admin.site.register(DepositProducts)
